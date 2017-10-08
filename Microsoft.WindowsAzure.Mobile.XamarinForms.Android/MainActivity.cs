a913371fd0b38a9a973955836597725655ba32ad
Rob Lyndon <rob.lyndon@gmail.com>
2017-10-08 15:44:06 +0100

    Adding Xamarin Forms package and trying to iron out Nuget package dependencies.

---
+++ Microsoft.WindowsAzure.Mobile.XamarinForms.Android/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Android.App;
+using Android.OS;
+using Xamarin.Android.NUnitLite;
+using Xamarin.Forms.Platform.Android;
+
+namespace Microsoft.WindowsAzure.Mobile.XamarinForms.Android
+{
+    [Activity(Label = "Microsoft.WindowsAzure.Mobile.XamarinForms.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+        }
+    }
+}
+
